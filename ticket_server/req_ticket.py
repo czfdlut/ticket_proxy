02b47e06633821a5e3a6e7f652501a0b0a19943a
czfdlut@163.com <czfdlut@163.com>
2019-03-24 12:11:42 +0800

    update

---
+++ ticket_server/req_ticket.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*
+#####################################################
+##                                  ##
+##         req_ticket 查询余票      ##
+##                                  ##
+#####################################################
+from util import request_query
+import tornado.web
+from datetime import datetime
+import json
+from tornado.concurrent import Future
+from tornado.httpclient import AsyncHTTPClient
+import tornado.httpclient
+
+class ReqTicketHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        self.url = self.config.get("REQ-API", "url")
+        self.timeout = self.config.getint("REQ-API", "timeout")
+
+    @property
+    def logger(self):
+        return self.application.logger
+
+    @property
+    def mysql_db(self):
+        return self.application.mysql_db
+
+    @property
+    def redis_client(self):
+        return self.application.redis_client
+
+    @property
+    def config(self):
+        return self.application.config
+
+    def finish_err_msg(self, msg):
+        self.set_header("Content-Type", "application/json;charset=UTF-8")
+        self.write({"errcode": -1, "errmsg": msg, "data": {}})
+        self.finish()
+
+    def content_type_from_headers(self):
+        for k,v in self.request.headers.items():
+            if k.lower() == "content-type":
+                return {k: v}
+        return {}
+
+    def check_request_param(self):
+        try:
+            param = json.loads(self.get_argument("param"))
+        except Exception as err:
+            return "Error: %s" % err, None
+
+        if "train_id" not in param:
+            return "Error: not found train_id", None
+
+        if "start_station_id" not in param:
+            return "Error: not found start_station_id", None
+
+        if "arrival_station_id" not in param:
+            return "Error: not found arrival_station_id", None
+
+        return None, param
+
+    def join_db_data(self, uid, param, server_resp_data):
+        hdata = {"uid": uid}
+        try:
+            resp_data = json.loads(server_resp_data)
+            hdata["errmsg"]  = resp_data["errmsg"]
+            try:
+                if resp_data["errcode"] != "OK":
+                    hdata["status"] = 0
+                else:
+                    hdata["status"] = 1
+            except:
+                hdata["status"] = 0
+        except Exception as err:
+            self.logger.error("Error: %s" % err)
+            return None
+
+        if "merchantCode" in param:
+            hdata["merchantCode"] = param["merchantCode"]
+
+        if "merchantName" in param:
+            hdata["merchantName"] = param["merchantName"]
+
+        if "bizNo" in param:
+            hdata["bizNo"] = param["bizNo"]
+
+        if "bizName" in param:
+            hdata["bizName"] = param["bizName"]
+
+        if "orderDate" in param:
+            hdata["orderDate"] = param["orderDate"]
+
+        if "orderNo" in param:
+            hdata["orderNo"] = param["orderNo"]
+
+        if "ticketPrices" in param:
+            hdata["ticketPrices"] = param["ticketPrices"]
+
+        if "mobile" in param:
+            hdata["mobile"] = param["mobile"]
+
+        if "requestID" in param:
+            hdata["requestID"] = param["requestID"]
+
+        if "merchantCode" in param:
+            hdata["merchantCode"] = param["merchantCode"]
+
+        #if "orderTicketFlow" in param:
+        #    hdata["orderTicketFlow"] = param["orderTicketFlow"]
+
+        #hdata["orderTicketFlow"] = resp_data["data"]["orderTicketFlow"]
+        hdata["updateTime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        return hdata
+
+    def set_response_header(self, headers):
+        for k, v in headers.items():
+            self.set_header(k, v)
+
+    def set_response_status(self, status):
+        self.set_status(status)
+
+    def get_uid_from_headers(self):
+        for k,v in self.request.headers.items():
+            if k.lower() == "ticket-uid":
+                return v
+        return None
+
+    def add_headers(self, headers, k, v):
+        is_exist_kv = False
+        for k ,v in headers.items():
+            if k.lower() == k.lower():
+                is_exist_kv = true
+
+        if is_exist_kv:
+            headers[k] = v
+
+    @tornado.gen.coroutine
+    def reqeust_proxy_server(self, headers, body):
+        return request_query(self.url, headers=headers, data=body, timeout=self.timeout)
+
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    def post(self):
+        start_time = datetime.now()
+
+        uid = self.get_uid_from_headers()
+        self.logger.info("ticket uid: %s url: %s " % (uid, self.url))
+        ## uid合法性校验
+        sql = "select balance from account_balance where uid='%s' and balance > 1.0 limit 1" % uid
+        qs = self.mysql_db.execute_query_sql(sql)
+        if qs is None or len(qs) == 0:
+            self.finish_err_msg(r"非法uid")
+            return
+
+        self.logger.info("account balance: %s" % qs[0][0])
+        
+        ##请求参数检查 
+        err, param = self.check_request_param()
+        if err is not None:
+            self.logger.error(err)
+            self.finish_err_msg(r"参数错误")
+            return
+
+        self.logger.info("reqeust_body: %s " % self.request.body)
+        
+        ##查询余票信息
+        sql = "select * from ticket where train_id='%s' and start_station_id='%s' and arrival_station_id='%s' \
+                limit 1" % (param['train_id'], param['start_station_id'], param['arrival_station_id'])
+        qs = self.mysql_db.execute_query_sql(sql)
+        if qs is None or len(qs) == 0:
+            self.finish_err_msg(r"非法参数")
+            return
+
+        self.logger.info("ticket info: %s" % qs[0][0])
+
+        ##通知client
+        resp_headers = {"Content-Type":"application/json"}
+        self.set_response_header(resp_headers)
+        self.set_response_status(200)
+
+        #hdata = self.join_db_data(uid, param, resp_data)
+        #self.logger.info("hdata: %s" % json.dumps(hdata))
+        resp_data = "query ticket success!"
+
+        self.write(resp_data)
+        self.finish()
+
+        self.logger.info("cost time: %s" %((datetime.now() - start_time)))
+
---
+++ ticket_server/sql/ticket.sql
@@ -0,0 +1,42 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : sssss
+ Source Server Type    : MySQL
+ Source Server Version : 50643
+ Source Host           : localhost:3306
+ Source Schema         : ticket_proxy
+
+ Target Server Type    : MySQL
+ Target Server Version : 50643
+ File Encoding         : 65001
+
+ Date: 04/03/2019 10:43:44
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for order_ticket
+-- ----------------------------
+DROP TABLE IF EXISTS `ticket`;
+CREATE TABLE `ticket`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `train_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '车次编号',
+  `train_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '车次名称',
+  `start_station_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '始发车站编号',
+  `start_station_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '始发车站名称',
+  `arrival_station_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '到达车站编号',
+  `arrival_station_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '到达车站名称',
+  `via_station` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '途经站信息',
+  `start_datetime` datetime(0) NULL COMMENT '始发时间',
+  `arrival_datetime` datetime(0) NULL COMMENT '到达时间',
+  `seat_type` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '座位席别',
+  `ticket_price` float(11, 0) NOT NULL COMMENT '票价',
+  `ticket_count` int(11) NOT NULL DEFAULT 0 COMMENT '余票数量',
+  `status` int(11) NULL DEFAULT NULL COMMENT '售票状态',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 142 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
