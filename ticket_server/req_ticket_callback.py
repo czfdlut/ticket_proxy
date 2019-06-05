b9f8ca90743c97b98bfe7a6b3f23be7fd2d95150
czfdlut@163.com <czfdlut@163.com>
2019-06-05 16:38:22 +0800

    update

--- ticket_server/req_ticket_callback.py
+++ ticket_server/req_ticket_callback.py
@@ -12,17 +12,18 @@ class ReqTicketCallBack(tornado.web.RequestHandler):
     def post(self):
         start_time = datetime.now()
        
-        resp_obj = {
+        resp_data = {
             "errcode" : "0",
             "errmsg" : {
                 "order_status" : "3",
                 "data" : "确认成功"
              }
         }
-        resp_data = json.dumps(resp_obj)
+        #post_data = bytes(json.dumps(resp_data), 'utf8')
+        post_data = json.dumps(resp_data)
         #resp_data = {"callback": "ok"} 
         self.set_header("Content-Type", "application/json;charset=UTF-8")
-        self.write(resp_data)
+        self.write(post_data)
         self.finish()
 
         self.logger.info("order ticket cost time: %s" %((datetime.now() - start_time)))
